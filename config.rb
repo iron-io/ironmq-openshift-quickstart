7d4bdd698ececc4a6a662f3d5a464b1fae7ed0ac
Chad Arimura <carimura@gmail.com>
2013-02-14 13:24:24 -0800

    creds

--- config.rb
+++ config.rb
@@ -1,8 +1,8 @@
 def get_client
   ### Set these to your IronMQ credentials ###
   config = {
-    token: 'put_token_here',
-    project_id: 'put_project_id_here'
+    token: 'WjZRmOk5BpkUkhtpxvpQno8Bxzk',
+    project_id: '4e25e1d25c0dd27801000283'
   }
   @ironmq ||= IronMQ::Client.new(token: config[:token], project_id: config[:project_id])
 end
